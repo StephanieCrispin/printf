52e659828c627cff2b2c7dd44cd28843f31c4bcf
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-22 17:05:16 +0100

    added files

--- _printf.c
+++ _printf.c
@@ -7,46 +7,46 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    unsigned k = 0, chars_printed = 0;
-    char c;
-    (void)c;
+	va_list args;
+	unsigned k = 0, chars_printed = 0;
+	char c;
+	(void)c;
 
-    va_start(args, format);
-    for (; format[k] != '\0'; k++)
-    {
-        if (format[k] != '%')
-        {
-            _putchar(format[k]);
-        }
-        else if (format[k] == '%' && format[k + 1] != 'c' && format[k + 1] != 's' && format[k + 1] != '%' && format[k + 1])
-        {
-            _putchar(format[k]);
-                }
-        else if (format[k + 1] == 'c')
-        {
-            _putchar(va_arg(args, int));
-            k++;
-        }
-        else if (format[k + 1] == 's')
-        {
-            int str_value = OurPutS(va_arg(args, char *));
-            k++;
-            chars_printed += (str_value - 1);
-        }
-        else if (format[k + 1] == '%')
-        {
-            _putchar('%');
-            k++;
-        }
-        else if ((format[k + 1] == 'd') || (format[k + 1] == 'i'))
-        {
-            handle_int(va_arg(args, int));
-            k++;
-        }
-        chars_printed += 1;
-    }
-    if (format[0] == '\n' || (format[0] == '%' && (!format[1])))
-        chars_printed = -1;
-    return (chars_printed);
+	va_start(args, format);
+	for (; format[k] != '\0'; k++)
+	{
+		if (format[k] != '%')
+		{
+			_putchar(format[k]);
+		}
+		else if (format[k] == '%' && format[k + 1] != 'c' && format[k + 1] != 's' && format[k + 1] != '%' && format[k + 1])
+		{
+			_putchar(format[k]);
+		}
+		else if (format[k + 1] == 'c')
+		{
+			_putchar(va_arg(args, int));
+			k++;
+		}
+		else if (format[k + 1] == 's')
+		{
+			int str_value = OurPutS(va_arg(args, char *));
+			k++;
+			chars_printed += (str_value - 1);
+		}
+		else if (format[k + 1] == '%')
+		{
+			_putchar('%');
+			k++;
+		}
+		else if ((format[k + 1] == 'd') || (format[k + 1] == 'i'))
+		{
+			handle_int(va_arg(args, int));
+			k++;
+		}
+		chars_printed += 1;
+	}
+	if (format[0] == '\n' || (format[0] == '%' && (!format[1])))
+		chars_printed = -1;
+	return (chars_printed);
 }
