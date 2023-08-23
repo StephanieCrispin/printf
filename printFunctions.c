eec963328994788fc49cdbcb71945aa0233908c1
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-23 05:09:29 +0100

    made betty updates

--- printFunctions.c
+++ printFunctions.c
@@ -3,46 +3,47 @@ int _write_character(char s, char buff[], int size);
 
 int printoutChar(va_list args, char buff[], int size)
 {
-    char s;
-    s = va_arg(args, int);
-    (void)size;
+	char s;
 
-    buff[0] = s;
-    buff[1] = '\0';
-    return write(1, buff, 1);
+	s = va_arg(args, int);
+	(void)size;
+
+	buff[0] = s;
+	buff[1] = '\0';
+	return (write(1, buff, 1));
 }
 
 int printoutString(va_list args, char buff[], int size)
 {
-    char *string;
-    int len = 0;
-    (void)size;
-    (void)buff;
-    (void)args;
+	char *string;
+	int len = 0;
+	(void)size;
+	(void)buff;
+	(void)args;
 
-    string = va_arg(args, char *);
-    if (string == NULL)
-        string = "(null)";
+	string = va_arg(args, char *);
+	if (string == NULL)
+		string = "(null)";
 
-    while (string[len] != '\0')
-    {
-        len++;
-    }
-    return (write(1, string, len));
+	while (string[len] != '\0')
+	{
+		len++;
+	}
+	return (write(1, string, len));
 }
 
 int printoutMod(va_list args, char buff[], int size)
 {
-    (void)size;
-    (void)buff;
-    (void)args;
-    return (write(1, "%%", 1));
+	(void)size;
+	(void)buff;
+	(void)args;
+	return (write(1, "%%", 1));
 }
 
 int _write_character(char s, char buff[], int size)
 {
-    (void)size;
-    buff[0] = s;
-    buff[1] = '\0';
-    return (write(1, buff, 1));
+	(void)size;
+	buff[0] = s;
+	buff[1] = '\0';
+	return (write(1, buff, 1));
 }
