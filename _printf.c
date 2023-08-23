a186f594d9a130b7bac42d20c3817993b3163806
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-08-23 14:15:41 +0100

    made updates

--- _printf.c
+++ _printf.c
@@ -11,19 +11,15 @@ int _printf(const char *format, ...)
 	int u = 0, buff_indicator = 0, chars_printed = 0, printed = 0, size;
 	va_list args;
 	char buff[SINGLE_BUFF];
+
 	if (format == NULL)
-	{
-		buff[0] = ' ';
-		buff_indicator = 1;
-		bufferPrint(buff, &buff_indicator);
 		return (-1);
-	}
-	else
 
-		va_start(args, format);
-
+	va_start(args, format);
 	for (; format && format[u] != '\0'; u++)
 	{
+		if (format[u] == '%' && format[u + 1] == ' ' && format[u + 2] == '%')
+			chars_printed = 1;
 		if (format[u] != '%')
 		{
 			buff[buff_indicator++] = format[u];
@@ -52,5 +48,8 @@ int _printf(const char *format, ...)
 	}
 	bufferPrint(buff, &buff_indicator);
 	va_end(args);
+
+	/*if ((format[0] == '%' && format[1] == ' '))
+		chars_printed = -1;*/
 	return (chars_printed);
 }
